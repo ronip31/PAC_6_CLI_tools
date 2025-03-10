9abf9f691a121d08e1372e6e5e5dcdbceb520321
Lucas de Carvalho Steffens <111320302+LucasCarvalhoSteffens@users.noreply.github.com>
2025-03-10 20:54:32 -0300

    Create testebasico.py

---
+++ Testes/testebasico.py
@@ -0,0 +1,22 @@
+"""
+Este é um docstring de módulo.
+Deve contar como docstring.
+"""
+
+class MinhaClasse:
+    """Esta classe faz algo interessante."""
+    
+    def metodo(self):
+        """Método simples."""
+        pass  # Comentário inline
+
+# Comentário isolado
+
+def funcao():
+    """
+    Esta função tem um docstring
+    que ocupa múltiplas linhas.
+    """
+    return 427
+
+# Outro comentário isolado
